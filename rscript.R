ed94f1bbe1af6a188f1ae6f893aecc188b4efb37
Latoe <LaeticiaCeline.Toe@UGent.be>
2017-09-15 10:31:22 +0200

    change author

---
+++ rscript.R
@@ -0,0 +1,47 @@
+1+100
+1+100
+#this is a comment
+2e6
+2^5
+2e-5
+log(1)
+log(le4, base=10)
+# looking up some details about funtions
+#?funtion
+?log 
+#Variables and assignment
+x <- 1/40
+x
+log(x)
+# vectorization
+1:5
+# vectorization
+# vectorization
+LS
+ls()
+ls(all.names=TRUE)
+y<- 2^x
+list<- ls()
+rm(list)
+#R packages
+mass <- 47.5
+age <- 122
+mass <- mass * 2.3
+age <- age - 20
+library (ggplot2)
+ vignette(package="ggplot2")
+ vignette("ggplot2-specs")
+rm(mass, age)
+int.vect<- c(1, 2, 3)
+combovect<- c("e", "d","f")
+getwd()
+cats <- read.csv("data/celin.csv")
+class(cats)
+class(cats$coat)
+class(cats$weight)
+str(cats$weight)
+class(cats)
+cats
+str(cats$weight)
+?read.csv
+read read.csv(cats, header=TRUE, sep",", )
